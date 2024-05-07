90e9000b6ac5821a7c792a3f5137ddb7b0543221
ashishghanghas <ashishaimt71college@gmail.com>
2024-05-08 03:01:21 +0530

    javascript practice

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var input=document.getElementsByClassName("input")
+var popup=document.getElementById("popup")
+
+var button=document.getElementById("submit").addEventListener('click',()=>{
+   
+
+        popup.style.display='block'
+
+   
+   
+
+   
+
+})
+var button2=document.getElementById("button2").addEventListener('click',()=>{
+    popup.style.display='none'
+
+})
+
+
